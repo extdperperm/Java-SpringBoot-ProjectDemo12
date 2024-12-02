7c99b366f2fa7b2324e7c4609048ffd712b3bdac
Daniel Pérez <118856521+extdperperm@users.noreply.github.com>
2024-12-02 10:24:53 +0000

    Add files via upload

---
+++ src/test/java/es/dsw/config/JavaSpringBootProjectDemo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsw.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootProjectDemo12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
